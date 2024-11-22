485e668cb2e250689376b9c6eed0ad8b87c9cc81
Magnus Nording <120574513+MagNording@users.noreply.github.com>
2024-11-22 19:26:34 +0100

    Update config.py

--- myflaskapp/config.py
+++ myflaskapp/config.py
@@ -6,7 +6,7 @@ class Config:
     # MySQL Settings
     DEBUG = True
     MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Tester1'
+    MYSQL_USER = ''
+    MYSQL_PASSWORD = ''
     MYSQL_DB = 'myflaskapp'
     MYSQL_CURSORCLASS = 'DictCursor'
